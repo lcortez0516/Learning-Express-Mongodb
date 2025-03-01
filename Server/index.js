670b3429d865b69239618d9c0c54d519d0ab3f3b
lcortez0516 <lestercortez0516@gmail.com>
2025-03-01 20:11:17 +0800

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -2,16 +2,25 @@ import express from "express";
 
 const app = express();
 
-app.get(
-  "/",
-  (req, res, next) => {
-    console.log(`first function`);
-    next();
-  },
-  (req, res) => {
-    console.log("second function");
-    res.send(`<h1>get</h1>`);
-  }
-);
+// app.get("/student", (req, res) => {
+//   res.send("get Student");
+// });
+// app.post("/student", (req, res) => {
+//   res.send("create Student");
+// });
+// app.put("/student", (req, res) => {
+//   res.send("update Student");
+// });
+// app.delete("/student", (req, res) => {
+//   res.send("delete Student");
+// });
 
-app.listen(8000, () => console.log(`hello`));
+//Refactore to use app.route()
+app
+  .route("/student")
+  .get((req, res) => res.send("Get student"))
+  .post((req, res) => res.send("create student"))
+  .put((req, res) => res.send(`update student`))
+  .delete((req, res) => res.send(`delete student`));
+
+app.listen(8000, () => console.log(`Server is running on port 8000`));
